95a1ac90bd2017618f4ac7eaa1efed931c41d9b6
Thomas Damas <tdamas@talent-bs.com>
2023-01-22 16:20:16 +0100

    feat: ajout de la map et de marker

--- edubee/lib/view/home/Home.dart
+++ edubee/lib/view/home/Home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,18 +14,99 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: Text("MAP"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'You are in the home page:',
-            ),
-          ],
-        ),
-      ),
+      body: Map(),
     );
   }
+}
+
+class Map extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(48.661717, 6.155612),
+        zoom: 16,
+      ),
+      nonRotatedChildren: [
+        AttributionWidget.defaultWidget(
+          source: 'OpenStreetMap contributors',
+          onSourceTapped: null,
+        ),
+      ],
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+        MarkerLayer(
+          markers: [
+            FlowerMarker(
+              flower: Flower(
+                name: "Érable champêtre",
+                image: "assets/Acer campestre.jpg",
+                location: LatLng(48.661717, 6.155612),
+              ),
+              width: 30,
+              height: 30,
+            ),
+            FlowerMarker(
+              flower: Flower(
+                name: "Érable champêtre",
+                image: "assets/Vinca minor.jpg",
+                location: LatLng(48.660717, 6.155612),
+              ),
+              width: 30,
+              height: 30,
+            ),
+            FlowerMarker(
+              flower: Flower(
+                name: "Érable champêtre",
+                image: "assets/Prunus avium.jpeg",
+                location: LatLng(48.661717, 6.156612),
+              ),
+              width: 30,
+              height: 30,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class Flower {
+  final String name;
+  final String image;
+  final LatLng location;
+
+  Flower({
+    required this.name,
+    required this.image,
+    required this.location,
+  });
+}
+
+class FlowerMarker extends Marker {
+  final Flower flower;
+
+  FlowerMarker({
+    required this.flower,
+    required double width,
+    required double height,
+  }) : super(
+          width: width,
+          height: height,
+          point: flower.location,
+          builder: (ctx) => Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Image.asset(flower.image, fit: BoxFit.cover)),
+          ),
+        );
 }
